6dd0c156e8dea2f962d9ae6abdf4218ddcd11f89
kjuiop <copying_y@naver.com>
2020-08-17 00:54:55 +0900

    react link

---
+++ module-api/src/main/java/com/gig/lookBook/api/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.gig.lookBook.api.controller;
+
+import com.gig.lookBook.core.exception.NotFoundException;
+import com.gig.lookBook.core.security.LoginUser;
+import org.json.simple.parser.ParseException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping
+public class IndexController {
+    
+    @RequestMapping
+    public ModelAndView index(@AuthenticationPrincipal LoginUser loginUser, HttpServletRequest request) throws NotFoundException, ParseException {
+
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+
+    @GetMapping("login")
+    public String login() { return "login"; }
+}
---
+++ module-api/src/main/java/com/gig/lookBook/api/controller/secure/SecureController.java
@@ -0,0 +1,12 @@
+package com.gig.lookBook.api.controller.secure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/secure")
+@RequiredArgsConstructor
+public class SecureController {
+
+}
